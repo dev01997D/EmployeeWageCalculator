dd4b664eef2f02b739e70c7b6ef208422afcc1e8
dev01997D <devnandan01997@gmail.com>
2020-09-24 18:05:30 +0530

    Added: Employee daily wage calculation functionality added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,6 +8,10 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Variables
+		int empWage=0;
+		int empHours=0;
+		
 		//Welcome statement
 		System.out.println("Welcome to Employee wage computation");
 		
@@ -15,10 +19,15 @@ public class EmployeeWage {
 		int empCheck=(int) (Math.floor(Math.random()*10)%2);
 		if (empCheck==IS_FULL_TIME) {
 			System.out.println("Full time employee present");
+			empHours=8;
 		}
 		else if (empCheck==IS_PART_TIME){
 			System.out.println("Part time employee present");
+			empHours=4;
 		}
+		//Daily wage calculation
+		empWage=empHours*20;
+		System.out.println("Employee daily wage is :"+empWage);
 
 	}
 
