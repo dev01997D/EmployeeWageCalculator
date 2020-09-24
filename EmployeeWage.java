c572b940bb28a40879bdbd241fccbddfe6b2cb79
dev01997D <devnandan01997@gmail.com>
2020-09-24 17:28:23 +0530

    Added: Welcome message added

---
+++ EmployeeWage.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.employeecheck;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Welcome statement
+		System.out.println("Welcome to Employee wage computation");
+
+	}
+
+}
